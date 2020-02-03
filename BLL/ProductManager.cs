00ed12508a428506b7906bda2702811c5438e7a9
Amit Gangurde <60591508+amitgangurde1@users.noreply.github.com>
2020-02-03 09:39:49 +0530

    Add files via upload

---
+++ BLL/ProductManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BOL;
+using DAL;
+
+
+namespace BLL
+{
+    public class ProductManager
+    {
+        public static List<Product> GetAll()
+        {
+            List<Product> products = null;
+            products = ProductDAL.GetAll();
+            return products;
+        }
+
+        public static Product GetBy(int id)
+        {
+            Product theProduct = null;
+            theProduct = ProductDAL.Get(id);
+            return theProduct;
+
+        }
+        public static List<Product> GetTop10()
+        {
+            List<Product> products = new List<Product>();
+            //.......
+            //.......
+
+            return products;
+
+        }
+    }
+
+}
