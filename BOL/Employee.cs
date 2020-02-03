10cc7a61ee48bcde8f984c177467ea847c5b7f99
Amit Gangurde <60591508+amitgangurde1@users.noreply.github.com>
2020-02-03 09:41:22 +0530

    Add files via upload

---
+++ BOL/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOL
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+    }
+}
